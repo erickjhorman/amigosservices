efe5070e31dcc30d8aa98590e12763ad4905653e
erick <erickjhorman.romero@globant.com>
2022-02-04 21:50:03 -0500

    feat: add rest template to talk with fraud microservice

--- customer/src/main/java/com/amigoscode/customer/controller/CustomerController.java
+++ customer/src/main/java/com/amigoscode/customer/controller/CustomerController.java
@@ -1,12 +1,14 @@
 package com.amigoscode.customer.controller;
 
 import com.amigoscode.customer.request.customer.CustomerRegistrationRequest;
+import com.amigoscode.customer.response.fraud.FraudCheckResponse;
 import com.amigoscode.customer.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @Slf4j
 @RestController
--- customer/src/main/java/com/amigoscode/customer/serviceimpl/CustomerServiceImpl.java
+++ customer/src/main/java/com/amigoscode/customer/serviceimpl/CustomerServiceImpl.java
@@ -1,16 +1,33 @@
 package com.amigoscode.customer.serviceimpl;
 
+import com.amigoscode.customer.domain.Customer;
 import com.amigoscode.customer.mapper.customer.CustomerMapper;
 import com.amigoscode.customer.repository.CustomerRepository;
 import com.amigoscode.customer.request.customer.CustomerRegistrationRequest;
+import com.amigoscode.customer.response.fraud.FraudCheckResponse;
 import com.amigoscode.customer.service.CustomerService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Service
-public record CustomerServiceImpl(CustomerMapper customerMapper, CustomerRepository customerRepository) implements CustomerService {
+public record CustomerServiceImpl(CustomerMapper customerMapper, CustomerRepository customerRepository,
+                                  RestTemplate restTemplate) implements CustomerService {
 
     @Override
     public void registerCustomer(CustomerRegistrationRequest request) {
-        customerRepository.save(customerMapper.carDtoToCustomer(request));
+        Customer customer = customerMapper.carDtoToCustomer(request);
+        customerRepository.saveAndFlush(customer);
+
+        Optional<FraudCheckResponse> fraudCheckResponse = Optional.ofNullable(restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        ));
+
+        if (fraudCheckResponse.isPresent() && fraudCheckResponse.get().isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
