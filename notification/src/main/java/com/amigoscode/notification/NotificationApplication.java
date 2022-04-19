cc98a4dc5a2cbb5a0889f60eb4c875b2009b15f4
erick <erickjhorman.romero@globant.com>
2022-04-19 16:20:28 -0500

    add publising message class

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -4,10 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.amigoscode.notification",
+                "com.amigoscode.amgp",
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+   /* @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+        return args -> producer.publish(
+                new Person("ALi",18),
+                notificationConfig.getInternalExchange(),
+                notificationConfig.getInternalNotificationRoutingKey());
+
+    }
+
+    record Person(String name, int age){}*/
 }
